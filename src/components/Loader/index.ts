1b7d3dce90d14a5a904da0c1e9a031d6413a31a2
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-05-18 15:26:36 +0530

    updated: loader component exports

--- src/components/Loader/index.ts
+++ src/components/Loader/index.ts
@@ -1,5 +1,11 @@
 import { LoaderBouncing } from './LoaderBouncing/LoaderBouncing';
 import { LoaderCometSpinner } from './LoaderCometSpinner/LoaderCometSpinner';
 import { LoaderDonutSpinner } from './LoaderDonutSpinner/LoaderDonutSpinner';
+import { LoaderInfiniteBar } from './LoaderInfiniteBar/LoaderInfiniteBar';
 
-export { LoaderBouncing, LoaderDonutSpinner, LoaderCometSpinner };
+export {
+  LoaderBouncing,
+  LoaderDonutSpinner,
+  LoaderCometSpinner,
+  LoaderInfiniteBar,
+};
