522fc502ff3f18e32c5e9f050475aee030b29359
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-05-18 15:24:00 +0530

    added: btnFloating export

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,5 +1,6 @@
 import { Btn } from './Btn/Btn';
 import { BtnInverted } from './BtnInverted/BtnInverted';
 import { BtnIcon } from './BtnIcon/BtnIcon';
+import { BtnFloating } from './BtnFloating/BtnFloating';
 
-export { Btn, BtnInverted, BtnIcon };
+export { Btn, BtnInverted, BtnIcon, BtnFloating };
