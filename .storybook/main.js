b9a6b8c7a015ae24958784b2d4f7b1741a932944
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-05-23 15:44:40 +0530

    updated: storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,31 @@
 module.exports = {
   stories: [
     '../src/**/*.stories.@(ts|tsx|js|jsx|mdx)',
-    '../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)'
+    '../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  addons: [
+    /** for mdx */
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+
+  // //////////////////// NEW CONFIG ////////////
+  // addons: [
+  //   {
+  //     name: '@storybook/addon-docs',
+  //     options: {
+  //       configureJSX: true,
+  //       babelOptions: {},
+  //       sourceLoaderOptions: null,
+  //     },
+  //   },
+  // ],
+  // ///////////////////// NEW CONFIG /////////////
+
+  // // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+    reactDocgen: 'react-docgen',
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,3 +3,15 @@ export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+//////////////////// NEW CONFIG //////////////////////
+// import { addParameters } from '@storybook/react';
+// import { DocsPage, DocsContainer } from '@storybook/addon-docs';
+
+// addParameters({
+//   docs: {
+//     container: DocsContainer,
+//     page: DocsPage,
+//   },
+// });
+/////////////////// NEW CONFIG //////////////////////
